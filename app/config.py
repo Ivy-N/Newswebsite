f42631d843648d44a69d6aa9be240e0b2db49617
Ivy-N <Ivy-N>
2020-07-05 18:44:33 +0300

    Create request.py. In it get API key from config object from app instance imported

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 class Config: #The parent Config class contains configurations that are used in both production and development stages. 
     
-    NEWSSOURCE_API_BASE_URL ='https://api.newsapi.org/3/newssource/{}?api_key={}'
+    NEWSSOURCE_API_BASE_URL ='https://api.newsapi.org/3/newssource/{}?api_key={}' # store the news source base URL inside the app/config file. We use the {} to represent sections in the URL that will be replaced with actual values
 
 
 class ProdConfig(Config): #  The ProdConfigsubclass contains configurations that are used in production stages of our application and inherits from the parent Config class.
---
+++ app/request.py
@@ -0,0 +1,3 @@
+from app import app # import the app instance
+
+api_key = app.config['NEWSSOURCE_API_KEY'] #from app instance we get the API key from the config object.
