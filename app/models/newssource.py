aaaad94b8ccdb8b3c1baa38821b87f73189f19a1
Ivy-N <Ivy-N>
2020-07-05 20:41:27 +0300

    reuters articles instead of popular

--- app/models/newssource.py
+++ app/models/newssource.py
@@ -2,8 +2,6 @@ class Newssource: # Newssource class to define Newssource Objects
 
     def __init__(self,id,title,description,urlToImage,publishedAt,url): #we define a Movie class and then we create an __init__ method and we pass in the six parameters we want inside our newssource objects.
 
-
-
         self.id =id
         self.title = title
         self.description = description
