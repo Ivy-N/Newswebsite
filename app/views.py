65529435f21d1bccd596ff4263f25bdfa2b01842
Ivy-N <Ivy-N>
2020-07-05 17:37:37 +0300

    include dynamic route for newssource on views.py

--- app/views.py
+++ app/views.py
@@ -7,3 +7,8 @@ def index(): #define the view function index()
  
     message = 'Hello World' #add a message variable
     return render_template('index.html',message = message) #use the render_template() function and pass in index.html file and message variable as argument. The first message on the left of the = sign, represents the variable in the template. While the one to the right represents the variable in our view function.
+
+@app.route('/newssource/<int:newssource_id>') #new route that has a newssource() view function. The part in the angle brackets <> is dynamic. And any route mapped to this will be passed. We changed the dynamic part to <int:newssource_id> to transform content to integer.
+def newssource(newssource_id): # returns a response of a newssource.html file. By default, dynamic parts are rendered as strings but they can be transformed to be of any type
+
+    return render_template('newssource.html',id = newssource_id) 
