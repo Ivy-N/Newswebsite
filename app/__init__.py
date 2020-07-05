2d24d9cef425fa680865b3c5b330d1715df2e6ed
Ivy-N <Ivy-N>
2020-07-05 16:54:37 +0300

    includes why comments

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
-from flask import Flask
+from flask import Flask #import Flask class from flask module
 
-app = Flask(__name__)
+app = Flask(__name__) #use Flask class to create app instance. Pass in the __name__ variable
 
-from app import view
+from app import views #import our views file from the app folder in order to create views
--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
-from flask import render_template
-from app import app
+from flask import render_template # import the render_template() function from Flask. This function takes in the name of a template file as the first argument. It then automatically searches for the template file in our app/templates/ sub directory and loads it.
+from app import app # import the app instance from the app folder.
 
-@app.route('/')
-def index():
 
-    return render_template('index.html')
+@app.route('/') #  create a route decorator @app.route('/')
+def index(): #define the view function index()
+
+    return render_template('index.html') # use the render_template() function and pass in index.html file
